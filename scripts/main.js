cda3359e67cd28a36b5d8ef04afdd990d9aaf3bb
Hassan Naeem <hassan@ralphandco.com>
2017-07-13 17:12:58 -0400

    Fixed styles and increased size for menu button

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
-// var helloWorld = function() {
-// 	console.log('Hello World');
-// };
-//
-// helloWorld();
-
 $(document).ready(() => {
 	$('.mobile-menu').click(() => {
 		$('.menu').toggleClass("open");
