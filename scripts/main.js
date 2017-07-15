dcb69cf72a7e4c585d9291190a1f071b4e085f8d
hnae6443 <hnae6443@gmail.com>
2017-07-15 16:35:16 -0400

    Added jquery for navbar scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,29 @@
 $(document).ready(() => {
 	$('.mobile-menu').click(() => {
 		$('.menu').toggleClass("open");
-	})
-})
+	});
+
+	$('.nav-home').click(() => {
+		$('html, body').animate({
+			scrollTop: $('.home-section').position().top
+		},'slow');
+	});
+
+	$('.nav-about').click(() => {
+		$('html, body').animate({
+			scrollTop: $('.about-section').position().top
+		},'slow');
+	});
+
+	$('.nav-portfolio').click(() => {
+		$('html, body').animate({
+			scrollTop: $('.portfolio-section').position().top
+		},'slow');
+	});
+
+	$('.nav-contact').click(() => {
+		$('html, body').animate({
+			scrollTop: $('.contact-section').position().top
+		},'slow');
+	});
+});
