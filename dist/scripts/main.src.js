4a0039c99935e7fd99e699a0dd8c8e788c1811c1
Hassan Naeem <hnae6443@gmail.com>
2018-11-05 12:31:07 -0500

    Format js

--- dist/scripts/main.src.js
+++ dist/scripts/main.src.js
@@ -1,29 +1,29 @@
 var mobileMenuLink = document.querySelector('.navbar__menu__mobile-link');
 var mobileMenuList = document.querySelector('.navbar__menu__list');
-var menuHome = document.querySelector('.navbar__menu__list__link--home')
-var menuAbout = document.querySelector('.navbar__menu__list__link--about')
-var menuPortfolio = document.querySelector('.navbar__menu__list__link--portfolio')
-var menuContact = document.querySelector('.navbar__menu__list__link--contact')
-var homeSection = document.querySelector('.home')
-var aboutSection = document.querySelector('.about')
-var portfolioSection = document.querySelector('.portfolio')
-var contactSection = document.querySelector('.contact')
-var navbar = document.querySelector('.navbar')
-var navbarLogo = document.querySelector('.navbar__logo')
-var expandArrow = document.querySelector('.home__expand-arrow')
-var resume = document.querySelector('.resume')
-var resumeOpenButton = document.querySelector('.footer__container__btn--resume')
-var resumeCloseButton = document.querySelector('.resume__close')
-var loadingScreen = document.querySelector('.loading')
+var menuHome = document.querySelector('.navbar__menu__list__link--home');
+var menuAbout = document.querySelector('.navbar__menu__list__link--about');
+var menuPortfolio = document.querySelector('.navbar__menu__list__link--portfolio');
+var menuContact = document.querySelector('.navbar__menu__list__link--contact');
+var homeSection = document.querySelector('.home');
+var aboutSection = document.querySelector('.about');
+var portfolioSection = document.querySelector('.portfolio');
+var contactSection = document.querySelector('.contact');
+var navbar = document.querySelector('.navbar');
+var navbarLogo = document.querySelector('.navbar__logo');
+var expandArrow = document.querySelector('.home__expand-arrow');
+var resume = document.querySelector('.resume');
+var resumeOpenButton = document.querySelector('.footer__container__btn--resume');
+var resumeCloseButton = document.querySelector('.resume__close');
+var loadingScreen = document.querySelector('.loading');
 
 function removeLoadingScreen() {
-  loadingScreen.style.display = 'none'
+  loadingScreen.style.display = 'none';
 }
 
 function scroll(destination) {
 	var startingPosition = window.pageYOffset;
-	var endingPosition = destination.offsetTop - navbar.offsetHeight
-	var difference = endingPosition - startingPosition
+	var endingPosition = destination.offsetTop - navbar.offsetHeight;
+	var difference = endingPosition - startingPosition;
 
 	window.scrollTo({
 		left: 0,
@@ -41,7 +41,7 @@ if (
 }
 
 navbarLogo.onclick = function() {
-	scroll(homeSection)
+	scroll(homeSection);
 }
 
 mobileMenuLink.onclick = function() {
@@ -69,15 +69,15 @@ menuContact.onclick = function () {
 }
 
 expandArrow.onclick = function() {
-	scroll(aboutSection)
+	scroll(aboutSection);
 }
 
 resumeOpenButton.onclick = function() {
 	resume.style.transform = 'none';
-	document.body.classList.add('resume__open')
+	document.body.classList.add('resume__open');
 }
 
 resumeCloseButton.onclick = function() {
-	resume.style.transform = 'translateX(100%)'
-	document.body.classList.remove('resume__open')
+	resume.style.transform = 'translateX(100%)';
+	document.body.classList.remove('resume__open');
 }
